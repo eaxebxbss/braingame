363d2fd6f6ba1ad28ea5b59454afb1fc874baea0
eaxebxbss <eaxebxbss@gmail.com>
2022-02-07 22:51:53 +0300

    add start game

--- braingame.pas
+++ braingame.pas
@@ -1,5 +1,10 @@
 program braingame;
+var
+   name: string;
 
 begin
-   writeln('Welcome to the Brain Games!')
+   writeln('Welcome to the Brain Games!');
+   write('May I have your name?: ');
+   read(name);
+   writeln('Hi,', name + '!')
 end.
