7b3a84058260609fac05d1b1e49498137733ab3b
eaxebxbss <eaxebxbss@gmail.com>
2022-02-08 18:51:31 +0300

    add greeting procedure

--- braingame.pas
+++ braingame.pas
@@ -1,10 +1,15 @@
 program braingame;
+
+procedure Greeting;
 var
    name: string;
-
-begin
+begin   
    writeln('Welcome to the Brain Games!');
    write('May I have your name?: ');
    read(name);
    writeln('Hi,', name + '!')
+end;
+
+begin
+   Greeting;
 end.
